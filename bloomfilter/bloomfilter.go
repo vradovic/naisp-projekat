205e558784299a4b396e67e41747cdfa74edfa64
Vladislav Radovic <vradovic522@gmail.com>
2022-11-21 21:41:02 +0100

    Add bloomfilter constructor

--- bloomfilter/bloomfilter.go
+++ bloomfilter/bloomfilter.go
@@ -1,1 +1,24 @@
 package bloomfilter
+
+import "math"
+
+type BloomFilter struct {
+	Data          []byte
+	HashFunctions []HashWithSeed
+}
+
+// Konstruktor za bloomfilter
+// expectedElements -> ocekivani broj elemenata
+// falsePositiveRate -> tolerancija na gresku
+func NewBloomFilter(expectedElements int, falsePositiveRate float64) *BloomFilter {
+	m := CalculateM(expectedElements, falsePositiveRate) // broj bitova
+	k := CalculateK(expectedElements, m)                 // broj hash funkcija
+
+	hashFunctions := CreateHashFunctions(k) // hash funkcije
+	bitsNum := math.Ceil(float64(m) / 8)    // broj bajtova
+	data := make([]byte, int(bitsNum))      // niz velicine m
+
+	b := BloomFilter{data, hashFunctions}
+
+	return &b
+}
