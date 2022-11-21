c82168e3e179294c20935ff1d01930bb405cc8f6
Vladislav Radovic <vradovic522@gmail.com>
2022-11-21 22:40:56 +0100

    Add bloomfilter add method

--- bloomfilter/bloomfilter.go
+++ bloomfilter/bloomfilter.go
@@ -3,6 +3,7 @@ package bloomfilter
 import "math"
 
 type BloomFilter struct {
+	M             uint
 	Data          []byte
 	HashFunctions []HashWithSeed
 }
@@ -18,7 +19,21 @@ func NewBloomFilter(expectedElements int, falsePositiveRate float64) *BloomFilte
 	bytesNum := math.Ceil(float64(m) / 8)   // broj bajtova
 	data := make([]byte, int(bytesNum))     // niz velicine m
 
-	b := BloomFilter{data, hashFunctions}
+	b := BloomFilter{m, data, hashFunctions}
 
 	return &b
+}
+
+// Dodavanje elementa u bloomfilter
+// data -> element za dodavanje
+func (b BloomFilter) Add(data []byte) {
+	for _, hashFunction := range b.HashFunctions {
+		hashed := hashFunction.Hash(data)
+		bit := hashed % uint64(b.M) // bit u nizu
+
+		targetByte := bit / 8     // bajt u kome se bit nalazi
+		bitMask := 1 << (bit % 8) // maska
+		index := int(targetByte)
+		b.Data[index] = b.Data[index] | byte(bitMask)
+	}
 }
