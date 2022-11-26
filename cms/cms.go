4f50789ddd1df5b47199248fbf601313f70d9ab6
Vladislav Radovic <vradovic522@gmail.com>
2022-11-26 10:17:48 +0100

    Add constructor

--- cms/cms.go
+++ cms/cms.go
@@ -6,3 +6,20 @@ type Cms struct {
 	Table         [][]byte
 	HashFunctions []HashWithSeed
 }
+
+func NewCms(epsilon, delta float64) *Cms {
+	m := CalculateM(epsilon)
+	k := CalculateK(delta)
+
+	// Matrica
+	table := make([][]byte, k)
+	var i uint
+	for i = 0; i < k; i++ {
+		table[i] = make([]byte, m)
+	}
+
+	hashFunctions := CreateHashFunctions(k)
+
+	cms := &Cms{m, k, table, hashFunctions}
+	return cms
+}
