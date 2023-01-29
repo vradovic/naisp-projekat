c7cb8f041b00f485c53f30df2c201037735d4611
Vladislav Radovic <vradovic522@gmail.com>
2023-01-29 12:59:52 +0100

    Add memtable struct

--- memtable/memtable.go
+++ memtable/memtable.go
@@ -1,1 +1,8 @@
 package memtable
+
+type Memtable struct {
+	maxSize   uint      // Maksimalna dozvoljena velicina
+	structure Structure // Struktura podataka (SkipList ili B stablo)
+}
+
+func (m Memtable) Flush()
