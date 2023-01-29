c90657e9036c0e08d185809fa93d4a195a31bcda
Vladislav Radovic <vradovic522@gmail.com>
2023-01-29 19:33:03 +0100

    Add constructor

--- memtable/memtable.go
+++ memtable/memtable.go
@@ -8,16 +8,37 @@ type Memtable struct {
 	structure   Structure // Struktura podataka (SkipList ili B stablo)
 }
 
-func (m Memtable) Flush() {
-	fmt.Println("Memtable flushed!")
+func NewMemtable(maxSize uint) *Memtable {
+	var currentSize uint = 0
+	structure := NewSkipList(5)
+	m := Memtable{currentSize, maxSize, structure}
+
+	return &m
 }
 
-func (m Memtable) Write(r Record) bool {
+func (m *Memtable) Flush() {
+	fmt.Println("Memtable flushed!")
+	// TODO: Potrebno flushovati u data fajl
+}
+
+func (m *Memtable) Write(r Record) bool {
 	success := m.structure.Write(r)
 
 	if success {
 		m.currentSize++
 	}
 
+	if m.currentSize > m.maxSize {
+		m.Flush()
+	}
+
 	return success
+}
+
+func (m *Memtable) Read(key string) []byte {
+	return m.structure.Read(key)
+}
+
+func (m *Memtable) Delete(key string) bool {
+	return m.structure.Delete(key)
 }
