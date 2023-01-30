9ef7644ca62ece03b729f2b67f5589257e06debe
Vladislav Radovic <vradovic522@gmail.com>
2023-01-30 11:35:51 +0100

    Add write function

--- io/write.go
+++ io/write.go
@@ -1,1 +1,19 @@
 package io
+
+import (
+	"github.com/vradovic/naisp-projekat/memtable"
+	"github.com/vradovic/naisp-projekat/record"
+	"github.com/vradovic/naisp-projekat/wal"
+)
+
+// PUT (Novi slog i brisanje sloga)
+func Write(key string, value []byte, timestamp int64, tombstone bool, log *wal.WAL, table *memtable.Memtable) bool {
+	err := log.Write([]byte(key), value, timestamp, tombstone)
+	if err != nil {
+		return false
+	}
+
+	record := record.Record{Key: key, Value: value, Timestamp: timestamp, Tombstone: tombstone}
+
+	return table.Write(record)
+}
