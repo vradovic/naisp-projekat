0361f3a989d299a987648f950b19cc435dde710d
Mihajlo Vujisic <vujisic.mihajlo02@gmail.com>
2023-01-31 12:20:11 +0100

    Evo vlado

--- structures/init.go
+++ structures/init.go
@@ -4,7 +4,6 @@ import (
 	"github.com/vradovic/naisp-projekat/cache"
 	"github.com/vradovic/naisp-projekat/config"
 	"github.com/vradovic/naisp-projekat/memtable"
-	"github.com/vradovic/naisp-projekat/record"
 )
 
 // STRUKTURE U MEMORIJI
@@ -15,6 +14,5 @@ var Cache *cache.Cache
 func Init() {
 	Memtable = memtable.NewMemtable(config.GlobalConfig.MemtableSize, config.GlobalConfig.StructureType)
 	Cache = cache.NewCache(config.GlobalConfig.CacheCapacity)
-	Cache.AddRecord(record.Record{"Mihajlo", []byte("Alo"), 123, false})
 
 }
