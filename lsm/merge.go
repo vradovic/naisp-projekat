85722282ac1d203a07450681545a18e5b7784bbe
Vladislav Radovic <vradovic522@gmail.com>
2023-02-02 17:52:05 +0100

    Add file open

--- lsm/merge.go
+++ lsm/merge.go
@@ -1,8 +1,24 @@
 package lsm
 
+import "os"
+
 // Spajanje dve ss tabele u jednu novu, first i second su putanje do tabela
-func MergeTables(first, second string) error {
+func MergeTables(first, second string) {
 	// ucitaj fajlove
 	// redom idi kroz fajlove i izvrsavaj merge
 	// napisi novu tabelu
+
+	firstFile, err := os.Open(first)
+	if err != nil {
+		panic(err)
+	}
+	defer firstFile.Close()
+
+	secondFile, err := os.Open(first)
+	if err != nil {
+		panic(err)
+	}
+	defer secondFile.Close()
+
+	// Pozicioniranje na data segment
 }
