1ed3fc0b3b91d7cedf7c64a77ff26e107d950bf1
Vladislav Radovic <vradovic522@gmail.com>
2023-02-02 23:27:28 +0100

    Add table reading

--- io/write.go
+++ io/write.go
@@ -3,8 +3,11 @@ package io
 import (
 	"github.com/vradovic/naisp-projekat/config"
 	"github.com/vradovic/naisp-projekat/record"
+	"github.com/vradovic/naisp-projekat/sstable"
 	"github.com/vradovic/naisp-projekat/structures"
 	"github.com/vradovic/naisp-projekat/wal"
+	"os"
+	"strings"
 )
 
 // PUT (Novi slog / azuriranje sloga)
@@ -36,6 +39,15 @@ func Get(key string) []byte {
 	if value != nil {
 		return value
 	}
+
+	vals := ReadTables([]string{key}, true)
+	if len(vals) > 0 {
+		if len(vals[0]) > 0 {
+			value = vals[0][0].Value
+			return value
+		}
+	}
+
 	return nil
 }
 
@@ -63,4 +75,45 @@ func Delete(key string, timestamp int64) bool {
 	}
 
 	return success
+}
+
+// Citanje ss tabela
+func ReadTables(keys []string, full bool) [][]record.Record {
+	tables, err := getTables()
+	if err != nil {
+		panic(err)
+	}
+
+	var records [][]record.Record
+
+	for _, table := range tables {
+		data := sstable.FindByKey(keys, "resources\\"+table, full)
+		records = append(records, data)
+	}
+
+	return records
+}
+
+// Pronalazenje putanja do tabela
+func getTables() ([]string, error) {
+	var files []string
+
+	dir, err := os.Open("resources")
+	defer dir.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	fileInfo, err := dir.Readdir(-1)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, file := range fileInfo {
+		if strings.Contains(file.Name(), "file") {
+			files = append(files, file.Name())
+		}
+	}
+
+	return files, nil
 }
