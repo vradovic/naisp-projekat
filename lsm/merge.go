641f0d0e98648bb8a00f5fff362717dd0b62075d
Vladislav Radovic <vradovic522@gmail.com>
2023-02-02 17:49:01 +0100

    Outline merge structure

--- lsm/merge.go
+++ lsm/merge.go
@@ -1,1 +1,8 @@
 package lsm
+
+// Spajanje dve ss tabele u jednu novu, first i second su putanje do tabela
+func MergeTables(first, second string) error {
+	// ucitaj fajlove
+	// redom idi kroz fajlove i izvrsavaj merge
+	// napisi novu tabelu
+}
