f4a65afa270013336ca7d6cae11d858e38d37a5c
milanovicmilos <2002mm9.mm@gmail.com>
2023-02-03 20:45:08 +0100

    vece promene citanje

--- sstable/sstableRead_test.go
+++ sstable/sstableRead_test.go
@@ -6,5 +6,5 @@ import (
 )
 
 func TestRead(t *testing.T) {
-	fmt.Println(findByKey([]string{"nol"}, "file_1675444267303371000.db", false))
+	fmt.Println(findByKey([]string{"m"}, "file_1675449068586085700.db", false))
 }
