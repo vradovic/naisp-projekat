8e61f63d9c144522d5da7a40c393ff528d16a63b
Vladislav Radovic <vradovic522@gmail.com>
2023-02-03 20:35:42 +0100

    Update sstable tests

--- sstable/sstableRead_test.go
+++ sstable/sstableRead_test.go
@@ -5,6 +5,22 @@ import (
 	"testing"
 )
 
+type readTest struct {
+	keys []string
+	full bool
+}
+
+var readTests = []readTest{
+	{[]string{"a", "z"}, true},
+	{[]string{"ekrem"}, true},
+	{[]string{"iv"}, false},
+	{[]string{"a", "f"}, true},
+	{[]string{"f", "z"}, true},
+}
+
 func TestRead(t *testing.T) {
-	fmt.Println(findByKey([]string{"nol"}, "file_1675444267303371000.db", false))
+	PATH := "resources\\file_1675452016305441200.db"
+	for _, test := range readTests {
+		fmt.Println(findByKey(test.keys, PATH, test.full))
+	}
 }
